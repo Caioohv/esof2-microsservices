2cfda172102d833f9deccc0f474540557a2105f9
Caio Vieira <caioviier@gmail.com>
2026-05-19 22:16:48 -0300

    feat: logging aux

--- services/auth-service/src/index.js
+++ services/auth-service/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const authRoutes = require('./routes/auth');
+const logger = require('./middlewares/logger');
 
 const app = express();
 app.use(express.json());
+app.use(logger);
 app.use(authRoutes);
 
 app.use((err, req, res, _next) => {
