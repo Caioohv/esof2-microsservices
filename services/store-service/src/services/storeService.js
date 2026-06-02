a7c01ef12bf36129b556694b9c6bd98f7153c6f4
Felipe Alexandre Vieira Mendes <145509208+FelipeAlexandre777@users.noreply.github.com>
2026-06-02 20:52:17 -0300

    Implement store service functions for CRUD operations

--- services/store-service/src/services/storeService.js
+++ services/store-service/src/services/storeService.js
@@ -1,1 +1,38 @@
+const repository = require('../repositories/storeRepository')
 
+async function createStore(data) {
+  return repository.createStore(
+    data.name,
+    data.description,
+    data.category
+  )
+}
+
+async function getStores() {
+  return repository.getStores()
+}
+
+async function getStoreById(id) {
+  return repository.getStoreById(id)
+}
+
+async function updateStore(id, data) {
+  return repository.updateStore(
+    id,
+    data.name,
+    data.description,
+    data.category
+  )
+}
+
+async function deleteStore(id) {
+  return repository.deleteStore(id)
+}
+
+module.exports = {
+  createStore,
+  getStores,
+  getStoreById,
+  updateStore,
+  deleteStore
+}
